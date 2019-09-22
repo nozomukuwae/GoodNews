3c44fe535c3e8def293291b7cd6d51827b3ea824
Nozomu Kuwae <nozomukuwae@gmail.com>
2019-09-22 18:10:11 +0900

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Nozomu Kuwae on 9/22/19.
+//  Copyright © 2019 Nozomu Kuwae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
